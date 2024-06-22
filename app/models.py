82f40749b2f08c64953c65be5da41e151059d0d9
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-06-22 12:58:54 +0300

    Update model to accept description

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, Float
-
+from sqlalchemy import Column, Integer, Float, String
 from .database import Base
 
 
 class TaxInfo(Base):
-    __tablename__ = "taxinfo"
+    __tablename__ = "tax_info"
 
     id = Column(Integer, primary_key=True, index=True)
     income = Column(Float, nullable=False)
     expenses = Column(Float, nullable=False)
     tax_amount = Column(Float, nullable=False)
     tax_rate = Column(Float, nullable=False)
+    description = Column(String, nullable=True)
