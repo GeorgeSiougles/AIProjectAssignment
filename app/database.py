fd12eb246bc39ce74a2be7ab8bd069177749b9e5
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-06-22 17:13:07 +0300

    Remove commented line

--- app/database.py
+++ app/database.py
@@ -8,7 +8,6 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env.local'))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-# DATABASE_URL = "sqlite:///./app/db.sqlite"
 
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
