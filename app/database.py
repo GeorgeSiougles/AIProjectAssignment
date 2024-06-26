e396da3cd2a6215ebdd7713056d24252468fe69a
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-06-26 17:10:04 +0300

    Add comments

--- app/database.py
+++ app/database.py
@@ -1,14 +1,26 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+import os  # Module for interacting with the operating system
+from sqlalchemy import create_engine  # Function to create a SQLAlchemy engine
+from sqlalchemy.ext.declarative import declarative_base  # Function to create a base class for ORM models
+from sqlalchemy.orm import sessionmaker  # Function to create a sessionmaker factory
+from dotenv import load_dotenv  # Function to load environment variables from a .env file
 
+# Define the base directory of the current file
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+# Load environment variables from a .env.local file located in the base directory
 load_dotenv(os.path.join(BASEDIR, '.env.local'))
 
+# Get the database URL from the environment variables; default to a SQLite database if not set
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test_db.sqlite")
 
+# Create a SQLAlchemy engine with the specified DATABASE_URL
+# For SQLite, the `connect_args` parameter is used to allow multi-threading
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Create a sessionmaker factory bound to the engine
+# autocommit=False ensures that sessions do not automatically commit after each transaction
+# autoflush=False ensures that changes are not automatically flushed to the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base class for all ORM models using the declarative_base function
 Base = declarative_base()
--- dockerfile
+++ dockerfile
@@ -1,23 +1,24 @@
 # Use an official Python runtime as a parent image
 FROM python:3.10-slim
 
-# Set the working directory in the container
+# Set the working directory in the container to /app
 WORKDIR /app
 
 # Copy the current directory contents into the container at /app
 COPY . /app
 
-# Copy the environment file
-COPY .env.local ./app/.env.local
+# Copy the environment file to the working directory in the container
+COPY .env.local /app/.env.local
 
 # Install any needed packages specified in requirements.txt
+# --no-cache-dir prevents the caching of package files to reduce image size
 RUN pip install --no-cache-dir -r requirements.txt 
 
 # Make port 8000 available to the world outside this container
 EXPOSE 8000
 
-# Define environment variable
+# Define environment variable to ensure stdout and stderr are sent straight to terminal (useful for debugging)
 ENV PYTHONUNBUFFERED=1
 
-# Run uvicorn server
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000","--reload"]
+# Run uvicorn server with live-reload enabled
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
